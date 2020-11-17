568dd76067706663e76b11507cf7481c3bbfc91b
darra <darra@192.168.0.108>
2020-11-17 20:08:14 +0000

    Final Commit, all comments added

--- src/main/java/Prj2/DriverClass.java
+++ src/main/java/Prj2/DriverClass.java
@@ -5,36 +5,42 @@ import assignment1project1.CourseProgramme;
 import org.joda.time.LocalDate;
 
 /**
-  * @author darra
+  * @author darraghoconnell (17371056)
  */
 public class DriverClass {
-    
+    //Driver Class for Student Registration System created in Project1
     public static void main(String[] args){
         DriverClass test= new DriverClass();
         test.registration1();
     }
     
     public void registration1(){
+        //Creating Studens
         Student student1 = new Student("John Johnson","19","20/02/2000","CT417, CT115","Electronic Engineering");
         Student student2 = new Student("Tom Thompson","18","09/09/1999","EE445","Electronic Engineering");
         Student student3 = new Student("Steve Stevenson","20","08/08/1998","EE445","Electronic Engineering");
-    
+        
+        //Creating Modules
         Module module1 = new Module("Software Engineering III","CT417", "Electronic Engineering");
         Module module2 = new Module("Digital Signal Processing","EE445","Electronic Engineering");
        
+        //Adding Students to Modules
         module1.addStudent(student1);
         module2.addStudent(student2);
         module2.addStudent(student3);
         
+        //Creating a Course
         LocalDate startdate = new LocalDate(2020, 8, 1);
         LocalDate enddate = new LocalDate(2021, 4, 1);
-    
-       CourseProgramme course1 = new CourseProgramme("Electronic Engineering",startdate,enddate);
+        CourseProgramme course1 = new CourseProgramme("Electronic Engineering",startdate,enddate);
+        
+        //Adding Modules and Students to Course
         course1.addModule(module1);
         course1.addModule(module2);
         course1.addStudents(module1);
         course1.addStudents(module2);
         
+        //Printing Output
         System.out.println("Course Modules\n"+course1.getModules());
         System.out.println("Course Students\n"+course1.getStudents());
         System.out.println("Students Modules and Courses: \n"+ student1.getUserName()+ "\n"+ student1.getModules() + "\n"+ student1.getCourses()+"\n\n" + "Students Modules and Courses: \n"+ student2.getUserName()+ "\n"+ student2.getModules()+"\n"+ student2.getCourses()+"\n"+"Students Modules and Courses: \n\n"+student3.getUserName()+ "\n"+ student3.getModules()+"\n"+ student3.getCourses()+"\n\n");
